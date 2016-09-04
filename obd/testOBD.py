fa6caf16f4227e818def3bb01fcb43b209dd2dd7
sommersoft <sommersoft@gmail.com>
2016-09-04 17:36:44 -0400

    new file:   obd/testOBD.py

---
+++ obd/testOBD.py
@@ -0,0 +1,12 @@
+import obd
+
+obd.logger.setLevel(obd.logging.DEBUG) # enables all debug information
+
+connection = obd.OBD("/dev/ttyUSB0") # auto-connects to USB or RF port
+
+cmd = {obd.commands.CONTROL_MODULE_VOLTAGE} # select an OBD command (sensor)
+
+response = connection.query(cmd) # send the command, and parse the response
+
+print(response.value) # returns unit-bearing values thanks to Pint
+print(response.value.to("volt")) # user-friendly unit conversions
