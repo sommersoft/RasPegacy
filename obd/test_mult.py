0981de8048499731d2e271d07c1e5e75d8d72d8e
sommersoft <sommersoft@users.noreply.github.com>
2016-09-06 22:03:42 -0400

    --

--- obd/test_mult.py
+++ obd/test_mult.py
@@ -2,7 +2,7 @@ import obd
 
 obd.logger.setLevel(obd.logging.DEBUG) # enables all debug information
 
-connection = obd.OBD() # auto-connects to USB or RF port
+connection = obd.OBD('/dev/ttyUSB0', 115200) # auto-connects to USB or RF port
 
 cmd = obd.commands.SPEED, obd.commands.RPM # select an OBD command (sensor)
 
