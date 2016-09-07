8dc4ea78a4a55b63a945f0bf5fdcb9947de452a6
sommersoft <sommersoft@users.noreply.github.com>
2016-09-06 21:21:08 -0400

    --

--- obd/testOBD.py
+++ obd/testOBD.py
@@ -4,7 +4,7 @@ obd.logger.setLevel(obd.logging.DEBUG) # enables all debug information
 
 connection = obd.OBD("/dev/ttyUSB0") # auto-connects to USB or RF port
 
-cmd = [obd.commands.CONTROL_MODULE_VOLTAGE] # select an OBD command (sensor)
+cmd = obd.commands.CONTROL_MODULE_VOLTAGE # select an OBD command (sensor)
 
 response = connection.query(cmd) # send the command, and parse the response
 
