6f6578be52759628bedaf6d733896f2cc44dcde4
sommersoft <sommersoft@users.noreply.github.com>
2016-09-06 21:25:12 -0400

    i'm stoopid

--- obd/test_mult.py
+++ obd/test_mult.py
@@ -6,7 +6,7 @@ connection = obd.OBD() # auto-connects to USB or RF port
 
 cmd = [obd.commands.SPEED, obd.commands.RPM] # select an OBD command (sensor)
 
-response = connection.query(cmd) # send the command, and parse the response
+response = connection.query_multi(cmd) # send the command, and parse the response
 
 #print(response.value) # returns unit-bearing values thanks to Pint
 #print(response.value.to("volt")) # user-friendly unit conversions
