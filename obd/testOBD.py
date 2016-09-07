34078d18a4a15466d273506370c297b404753362
sommersoft <sommersoft@users.noreply.github.com>
2016-09-06 21:17:42 -0400

    --

--- obd/testOBD.py
+++ obd/testOBD.py
@@ -4,9 +4,9 @@ obd.logger.setLevel(obd.logging.DEBUG) # enables all debug information
 
 connection = obd.OBD("/dev/ttyUSB0") # auto-connects to USB or RF port
 
-cmd = {obd.commands.CONTROL_MODULE_VOLTAGE} # select an OBD command (sensor)
+cmd = [obd.commands.CONTROL_MODULE_VOLTAGE] # select an OBD command (sensor)
 
 response = connection.query(cmd) # send the command, and parse the response
 
 print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("volt")) # user-friendly unit conversions
+#print(response.value.to("volt")) # user-friendly unit conversions
