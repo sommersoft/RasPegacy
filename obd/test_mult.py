12ca79c7e6e3188fd80791fd14520c7065e2c68f
sommersoft <sommersoft@users.noreply.github.com>
2016-09-17 12:04:36 -0400

    --

--- obd/test_mult.py
+++ obd/test_mult.py
@@ -4,17 +4,18 @@ obd.logger.setLevel(obd.logging.DEBUG) # enables all debug information
 
 connection = obd.OBD('/dev/ttyUSB0', 115200) # auto-connects to USB or RF port
 
-cmd = [obd.commands.SPEED, obd.commands.RPM, obd.commands.INTAKE_PRESSURE] # select an OBD command (sensor)
+cmd = [obd.commands.SPEED, obd.commands.RPM, obd.commands.INTAKE_PRESSURE, obd.commands.[1][10]] # select an OBD command (sensor)
 
 #response = connection.query_multi(False, obd.commands.SPEED, obd.commands.RPM) # send the command, and parse the response
 #response = connection.query_multi(cmd) # send the command, and parse the response
 while True:  
   try:
-    speed, rpm, intake = connection.query_multi(*cmd)
+    speed, rpm, intake, maf = connection.query_multi(*cmd)
     
     print(rpm.value)
     print(speed.value.to('mph'))
     print(intake.value.to('psi'))
+    print(maf.value)
     time.sleep(0.5)
     
   except KeyboardInterrupt:
