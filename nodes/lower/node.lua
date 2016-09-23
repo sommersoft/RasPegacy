7ad3a868665b50afbede64e9a6818224f30c5adf
sommersoft <sommersoft@users.noreply.github.com>
2016-09-23 17:54:25 -0400

    fixed data_mappers from start.py

--- nodes/lower/node.lua
+++ nodes/lower/node.lua
@@ -14,15 +14,15 @@ local maf = "--"
 util.data_mapper{
     ["set/coolant"] = function(new_cool)
         cool = tostring(new_cool)
-        print("cool", cool)
+        --print("cool", cool)
     end;
     ["set/tps"] = function(new_tps)
         tps = tostring(new_tps)
-        print("tps", tps)
+        --print("tps", tps)
     end;
     ["set/maf"] = function(new_maf)
         maf = tostring(new_maf)
-        print("maf", maf)
+        --print("maf", maf)
     end;
 }
 
@@ -34,7 +34,7 @@ function node.render()
     --end
 
     -- Write status message
-    font:write(300, 10, "LOWER VALUES", 20, 1, 1, 1, 1)
+    font:write(300, 10, "LOWER VALUES", 24, 1, 1, 1, 1)
     font:write(30, 30, "COOLANT TEMP: " .. cool, 20, 1, 1, 1, 1)
     font:write(80, 30, "MAF: " .. maf, 20, 1, 1, 1, 1)
     font:write(130, 30, "THROTTLE POS: " .. tps, 20, 1, 1, 1, 1)
